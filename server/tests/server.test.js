ac6295eb02fc103fc7ca73c792996221671a51b5
artitanna_rxsense <atanna@rxsense.com>
2020-09-23 14:10:53 +0530

    Test case for get /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,17 @@ var request = require('supertest');
 var {Todo} = require('./../models/todo');
 var {app} = require('./../server');
 
+var todos = [{
+  text: "First test"
+},
+{
+  text: "Second test"
+}]
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos).then(() => done());
+  });
 });
 
 describe('post /todos', () => {
@@ -23,7 +32,7 @@ describe('post /todos', () => {
         return done(err);
       }
 
-      Todo.find().then((todos) => {
+      Todo.find({text}).then((todos) => {
         expect(todos.length).toBe(1);
         expect(todos[0].text).toBe(text);
         done();
@@ -42,9 +51,20 @@ describe('post /todos', () => {
       }
 
       Todo.find().then((todos) => {
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(2);
         done();
       }).catch((e) => done(e));
     })
   })
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app).get('/todos')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todos.length).toBe(2);
+    });
+    .end(done);
+  });
 })
