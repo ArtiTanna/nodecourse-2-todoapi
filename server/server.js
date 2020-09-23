57f6fa4fc7d1e4ba6b859e7305dc9eb0e38e3002
artitanna_rxsense <atanna@rxsense.com>
2020-09-23 12:21:33 +0530

    server files test cases

--- server/server.js
+++ server/server.js
@@ -22,14 +22,15 @@ app.post('/todos', (req, res) => {
   })
 });
 
-app.get('/todos', (req, res) => {
-
-});
+// app.get('/todos', (req, res) => {
+//
+// });
 
 app.listen(3000, () => {
   console.log("started on port 3000");
 });
 
+module.exports = {app};
 
 // var newTodo = new Todo({
 //   text: "Cook Dinner"
